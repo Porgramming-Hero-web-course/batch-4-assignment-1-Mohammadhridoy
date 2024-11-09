4136818ac483633d615902feb8b0a6c17b6eecb9
Mohammadhridoy <mdridoykha019@gmail.com>
2024-11-09 21:42:28 +0600

    solve problem number 7

---
+++ problem-7.ts
@@ -0,0 +1,23 @@
+{
+    class Car {
+        make: string;
+        model:string; 
+        year:number; 
+        constructor( make: string, model:string, year:number,){
+            this.make = make;
+            this.model=model; 
+            this.year = year
+        }
+
+        getCarAge(){
+            const currentYear:number = new Date().getFullYear()
+            const  carAges = currentYear - this.year
+
+            console.log(`${carAges} (assuming current year is 2024)`)
+        }
+
+    }
+
+
+
+}
